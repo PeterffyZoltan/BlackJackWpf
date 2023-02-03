d484adde1eb03dd7a0331698ed93e234cbc113ef
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2023-02-03 09:22:02 +0100

    displayingCards

--- FeketeJanos/FeketeJanos/Kartya.cs
+++ FeketeJanos/FeketeJanos/Kartya.cs
@@ -8,7 +8,23 @@ namespace FeketeJanos
 {
     class Kartya
     {
-        public int Ertek { get; set; }
-        public string src { get; set; }
+
+		public int MyProperty
+		{
+			get {
+				string[] s = src.Split('.');
+				return int.Parse(s[0])%13 == 0? 13 : int.Parse(s[0]) % 13;
+			}
+		}
+
+		public string src { get; set; }
+
+		
+
+
+		public Kartya(int num)
+		{
+			src = $"{num}.png";
+		}
     }
 }
--- FeketeJanos/FeketeJanos/MainWindow.xaml.cs
+++ FeketeJanos/FeketeJanos/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FeketeJanos
 {
@@ -20,9 +21,53 @@ namespace FeketeJanos
     /// </summary>
     public partial class MainWindow : Window
     {
+        Kartya[] selectedCards = new Kartya[4];
+        List<Kartya> kartyak = new List<Kartya>();
         public MainWindow()
         {
             InitializeComponent();
+            
+            for (int i = 1; i <= 52; i++)
+            {
+                Kartya k = new Kartya(i);
+                kartyak.Add(k);
+                
+            }
+            Play();
+        }
+
+        private void Play()
+        {
+            getRandomCards();
+            displayCards();
+        }
+
+        private void getRandomCards()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Random r = new Random();
+                int kIndex =  r.Next(0, kartyak.Count-1);
+                selectedCards[i] = kartyak[kIndex];
+                kartyak.RemoveAt(kIndex);
+
+
+            }
+        }
+
+        private void displayCards()
+        {
+            
+                
+                ImgLap1.Source = new BitmapImage(new Uri(selectedCards[0].src, UriKind.Absolute));
+            ImgLap2.Source = new BitmapImage(new Uri(selectedCards[1].src, UriKind.Absolute));
+            ImgLap3.Source = new BitmapImage(new Uri(selectedCards[2].src, UriKind.Absolute));
+            ImgLap4.Source = new BitmapImage(new Uri(selectedCards[3].src, UriKind.Absolute));
+
+
+
+
+
 
         }
     }
